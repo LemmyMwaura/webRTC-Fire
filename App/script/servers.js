d71221c893221ed26d633a8dabf5dde164456c7c
lemmy mwaura <lemminton@gmail.com>
2023-01-10 18:02:51 +0300

    feat: add stun servers

---
+++ App/script/servers.js
@@ -0,0 +1,14 @@
+export const servers = {
+  iceServers: [
+    {
+      urls: [
+        "stun:stun1.l.google.com:19302",
+        "stun:stun2.l.google.com:19302",
+        "stun:stun3.l.google.com:19302",
+        "stun:stun4.l.google.com:19302",
+        "stun:stunserver.org:3478",
+      ],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+}
